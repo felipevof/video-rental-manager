a384ddd7adf3b3acbdccb655f65196fbe7c01cb1
Felipe <felipeevieiraa@gmail.com>
2018-05-30 09:56:10 -0300

    Adding the REST api

--- domain/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-    In the data source module/layer, the specifications and mappings for
-    the data used by the application are defined.
-
-    In short, any database table specification, ORM mapping, repositories or querying
-    mechanisms should be placed in this module.
-"""
